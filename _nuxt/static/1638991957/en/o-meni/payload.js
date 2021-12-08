a3fcf5721adb0b73de5eec3ca1261f1ebfe57106
suncica-negra <suncica.negra@gmail.com>
2021-12-08 20:36:46 +0100

    a282289

---
+++ _nuxt/static/1638991957/en/o-meni/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/en/o-meni", {data:[{_img:{"/personal-web-page/_ipx/f_webp,q_100,s_100x100/images/me.jpg":"\u002Fpersonal-web-page\u002F_nuxt\u002Fimage\u002F7d702f.webp","/personal-web-page/_ipx/f_webp,q_100,s_60x60/images/cro-lang.png":"\u002Fpersonal-web-page\u002F_nuxt\u002Fimage\u002F185adf.webp"}}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1638991957/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fo-meni","\u002Fen","\u002Fkontakt","\u002Fprojekti","\u002Fen\u002Fo-meni","\u002Fen\u002Fkontakt","\u002Fen\u002Fprojekti"]})
